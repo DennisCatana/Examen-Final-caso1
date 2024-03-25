f5d348b711480e47eaf6160f281cd1ba796733c2
Dennis Cataña <117744033+DennisCatana@users.noreply.github.com>
2024-03-25 09:59:09 -0500

    CRUD de clientes listo

--- back/src/routers/clienteRutas.js
+++ back/src/routers/clienteRutas.js
@@ -1,14 +1,11 @@
 import {Router} from 'express'
 import verificarAutenticacion from "../middlewares/autenticacion.js";
+
 import{listarCliente,
     detalleCliente,
     registrarCliente,
     actualizarCliente,
     eliminarCliente} from "../controllers/clienteControlador.js";
-
-
-
-
 const router = Router()
 
 //listar cliente
